5ed48131dc4e89d3bbea269f58cbf987539fe986
MRzNone <whzeng98@gmail.com>
2021-07-14 18:27:13 +0800

    add long desc to fix project desc on pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 # setup.py
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='VectorGym',
     version='0.1.0',
@@ -12,6 +19,8 @@ setup(
     keywords=['gym', 'multiprocess', 'parallel', 'simple'],
     download_url=
     'https://github.com/MRzNone/VectorGym/archive/refs/tags/v0.1.0.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'numpy',
         'gym',
